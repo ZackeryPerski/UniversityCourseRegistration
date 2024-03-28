f1b2c6bfdaf3f96e0bca9a8500866cb0f3022f87
Mohammad <mohammadimaan484@gmail.com>
2024-03-27 22:09:54 -0400

    added instructor my_courses page stuff

--- app/controllers/main_site_controller.rb
+++ app/controllers/main_site_controller.rb
@@ -3,12 +3,16 @@ class MainSiteController < ApplicationController
   end
 
   def my_courses
-    @user = Student.find(6).user
+    @user = Instructor.find(1).user
     @person = @user.person
     current_semester_args = { semester: 'Winter', year: 2024 }
     semester_sort_order = {'Winter' => 1, 'Summer' => 2, 'Fall' => 3}
 
     @current_sections = @person.sections.where(current_semester_args).includes(:course)
+
+    if @user.instructor?
+      @current_sections = @current_sections.includes(sections_students: :student).all
+    end
 
     @past_sections = @person.sections.where.not(current_semester_args).includes(:course)
     @past_sections = @past_sections.sort_by { |s| [-s.year, semester_sort_order[s.semester]] }
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,8 @@ class Student < ApplicationRecord
   majors = %w[computer_science mathematics physics]
 
   enum major: majors.map { |major| [major.to_sym, major] }.to_h
+
+  def full_name
+    "#{user.first_name} #{user.last_name}"
+  end
 end
